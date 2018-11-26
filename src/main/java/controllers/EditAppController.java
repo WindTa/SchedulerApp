b46e17034e5ee22e806556bb6d31e91f166d34de
WindTa <johnvoy68@gmail.com>
2018-11-26 15:49:01 -0800

    Created scenes
    Created scenes for making, editing and cancelling appointments. Have not
    created functions yet

--- src/main/java/controllers/EditAppController.java
+++ src/main/java/controllers/EditAppController.java
@@ -1,4 +1,17 @@
 package main.java.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import main.java.Main;
+
+import java.io.IOException;
+
 public class EditAppController {
+    public void homeClick(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main/resources/view/Home.fxml"));
+
+        Main.window.getScene().setRoot(root);
+        Main.window.show();
+    }
 }
